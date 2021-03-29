eaff6e310b44307e90a94c9c5250abdaf36f3649
Jard Krm <krjared@gmail.com>
2021-03-29 14:24:56 -0500

    props in react

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
 import React from "react"
 import TodoItem from "./components/TodoItem"
+import ContactCard from "./components/ContactCard"
 
 
 function App (){
     return (
         <div>
-        <TodoItem/>
+            <ContactCard 
+            contact = 
+            {{
+             name:"Mr.Whiskerson",
+             imageUrl:"http://placekitten.com/300/200",
+             phone:"(212) 555-1234",
+             email:"mr.whiskaz@catnap.meow" 
+            }}
+            />
 
-        <TodoItem/>
-
-        <TodoItem/>
-
-        <TodoItem/>
+            <ContactCard
+            contact = {{
+                name:"Fluffykins",
+                imageUrl:"http://placekitten.com/400/200",
+                phone:"(212) 555-2345",
+                email:"fluff@me.com"
+            }}
+            />
+            
+    
 
         </div>
        
